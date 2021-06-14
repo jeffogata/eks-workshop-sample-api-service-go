c5d73eb36dd258ae2cdc48e5841ad777c6831a51
Jeff Ogata <jeffogata@users.noreply.github.com>
2021-06-14 16:59:23 -0500

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello k8s on AWS EKS!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
